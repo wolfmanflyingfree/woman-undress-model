64dc857a8171defe25ce6887d70bca7b823bf36a
wolfmanflyingfree <bradlallen@yahoo.com>
2026-02-19 19:18:38 -0600

    Create pinokio.js

---
+++ pinokio.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "Woman Undress & Model",
+  description: "Beautiful woman slowly undresses and models seductively for the camera. Adapts to your starting image.",
+  menu: [
+    {
+      html: "<i class='fa-solid fa-camera'></i> Start Undress Session",
+      href: "start.json?run=true&fullscreen=true"
+    }
+  ]
+}
